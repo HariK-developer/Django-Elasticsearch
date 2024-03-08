6a79b2269a7dd79bbd65a2d4108c4e547373c857
HariK-developer <hariswdeveloper@gmail.com>
2024-03-08 09:00:53 +0530

    initial-commit

---
+++ blog/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from blog.models import Category,Article
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id","username","first_name","last_name")
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+        
+        
+class ArticleSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+    categories = CategorySerializer(many=True)
+    
+    class Meta:
+        model = Article
+        fields = "__all__"
+    
+        
+        
